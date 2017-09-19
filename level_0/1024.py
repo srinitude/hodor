caf41de8fbbb1a361ef7c0cd46dc4cfd59787206
Kiren Srinivasan <srinitude@gmail.com>
2017-09-19 22:21:55 +0000

    Makes POST requests in Python

---
+++ level_0/1024.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Sending a POST request to Hodor resource 1024 times
+"""
+import requests
+
+
+for i in range(1024):
+    'id=139&holdthedoor=Submit'
+    hodor_data = {"id": "139", "holdthedoor": "Submit"}
+    requests.post("http://158.69.76.135/level0.php", data = hodor_data)
