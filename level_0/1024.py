a087946c16e6bea5a872acf1946cd7fde062d1a4
Kiren Srinivasan <srinitude@gmail.com>
2017-09-19 22:25:24 +0000

    Takes out junk text from Python script

--- level_0/1024.py
+++ level_0/1024.py
@@ -6,6 +6,5 @@ import requests
 
 
 for i in range(1024):
-    'id=139&holdthedoor=Submit'
     hodor_data = {"id": "139", "holdthedoor": "Submit"}
     requests.post("http://158.69.76.135/level0.php", data = hodor_data)
