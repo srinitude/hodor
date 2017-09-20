2e1bbf5ab7ad18c3357197428300bdefe8f306d8
Kiren Srinivasan <srinitude@gmail.com>
2017-09-20 19:33:33 +0000

    Adds final level 2 Hodor

--- level_2/1024.py
+++ level_2/1024.py
@@ -13,7 +13,7 @@ def update_cookie_and_payload(res, cookie, payload):
     Updates the cookie and payload as each vote gets cast
     """
     cookie["HoldTheDoor"] = res.cookies["HoldTheDoor"]
-    tree = html.fromstring(res.content)
+    tree = html.fromstring(res.text)
     key = str(tree.xpath('//input[@name="key"]/@value')[0])
     payload["key"] = key
 
@@ -28,22 +28,24 @@ def cast_vote(url, cookie, payload):
     """
     Casts a vote as if you were on Windows
     """
-    user_agent = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
     header = {
+        "Referer": url,
         "User-Agent": user_agent
     }
     res = requests.post(url, data=payload, cookies=cookie, headers=header)
     return res
 
-cookie = {}
-payload = {
+hodor_cookie = {}
+hodor_payload = {
     "id": "139",
-    "holdthedoor": "Submit"
+    "holdthedoor": "Submit Query"
 }
 endpoint = "http://158.69.76.135/level2.php"
 
-get_initial_data(endpoint, cookie, payload)
+get_initial_data(endpoint, hodor_cookie, hodor_payload)
 
 for i in range(1024):
-    res = cast_vote(endpoint, cookie, payload)
-    update_cookie_and_payload(res, cookie, payload)
+    res = cast_vote(endpoint, hodor_cookie, hodor_payload)
+    print("Vote #{:d} casted".format(i))
+    update_cookie_and_payload(res, hodor_cookie, hodor_payload)
