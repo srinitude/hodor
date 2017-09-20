7f8b200ef19ef6d77cde86e7885a7bab998ec73e
Kiren Srinivasan <srinitude@gmail.com>
2017-09-20 00:56:17 +0000

    Adds feedback that vote has been casted

--- level_1/4096.py
+++ level_1/4096.py
@@ -15,6 +15,7 @@ hodor_key = str(tree.xpath('//input[@name="key"]/@value')[0])
 for i in range(4096):
     hodor_data = {"id": "139", "holdthedoor": "Submit", "key": hodor_key}
     res = requests.post(url, data=hodor_data, cookies=hodor_cookie)
+    print("Vote #{:d} casted".format(i))
     hodor_cookie["HoldTheDoor"] = res.cookies["HoldTheDoor"]
     tree = html.fromstring(res.text)
     hodor_key = str(tree.xpath('//input[@name="key"]/@value')[0])
